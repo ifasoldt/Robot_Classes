bc579117e6b2e8444a038cd962433072dbd6d743
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-06-22 23:07:55 -0400

    finished Robot homework.

--- robot.rb
+++ robot.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 class Robot
   attr_accessor :name, :height
   def initialize(name, height = 10)
@@ -26,38 +27,20 @@ end
 
 class ActorUnit < Robot
   def change_name(newname)
-    @name = newname
+    self.name = newname
   end
 end
 
-
-jake = Robot.new("Jake")
-puts jake.say_name
-
-chris = BendingUnit.new("Chris")
-chris.bend("pipe")
-
-bob = ActorUnit.new("Bob")
-puts bob.name
-bob.change_name("Billy Bob")
-puts bob.name
-
 our_class = ["Isaiah", "Marie", "Sean", "Daniel"]
 
-classmates = []
-
+robot_classmates = []
 
 our_class.each do |x|
-classmates << Robot.new(x)
+robot_classmates << Robot.new(x)
 end
-
-array_of_classes = ["Robot", "ActorUnit", "BendingUnit"]
 
 robot_class = our_class.collect{|x| Robot.new(x)}
 
-puts robot_class[1].inspect
-puts classmates[0].inspect
+array_of_classes = [Robot, ActorUnit, BendingUnit]
 
-puts robot_class[2].height
-
-puts jake.asdfsdf
+random_object_class = our_class.collect{|x| array_of_classes.shuffle[0].new(x)}
